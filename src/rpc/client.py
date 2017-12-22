4fe622fd06b404b98cbfac7d32229a2be3764283
aghabayli <aytaj.aghabayli@gmail.com>
2017-12-22 14:22:09 +0200

    rpc_client

--- src/rpc/client.py
+++ src/rpc/client.py
@@ -14,20 +14,10 @@ def new_player(nickname, client_address, client_port):
     except KeyboardInterrupt:
         exit(0)
 
-def new_session(self, client_id, desired_player):
-    client = self.__clienlist[client_id]
-    game = {}
-    session = {"session_id": self.__session_numerator, "clients": [client], "game": game,
-                   "desired_player": desired_player, "score_board": dict.fromKeys([client_id])}
-    self.__session_numerator += 1
-    self.__sessionlist.append(session)
-    return session["session_id"]
+def new_session():
 
-
-        
 def join_session():
-
-
+    
 def is_seesion_ready():
 
 def process_game_move():
