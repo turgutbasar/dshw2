ec214d04f06e77fdf51ccc811801b809178a3cff
Başar Turgut <basar@trdlab.com>
2018-01-01 08:46:12 +0300

    Game play

--- src/client.py
+++ src/client.py
@@ -1,186 +1,35 @@
 # Imports----------------------------------------------------------------------
-from Tkinter import *
-import tkMessageBox
-from gui.game_screen import SudokuApp
-from rpc.client import get_session_list, new_player, new_session, join_session
+from argparse import ArgumentParser # Parsing command line arguments
+from rpc.common import DEFAULT_SERVER_INET_ADDR, DEFAULT_SERVER_PORT
 from os.path import abspath, sep
 from sys import path,argv
-from sudoku.game import Sudoku
+from gui.gui import GUI
 
-# Vars -----------------------------------------------------------------
-nickname = ""
-current_frame = None
-sudoku_app = None
-proxy = None
-client_id = None
-session = None
+import logging
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s (%(threadName)-2s) %(message)s')
+LOG = logging.getLogger()
 
-# Methods -----------------------------------------------------------------
-def error_message(message):
-    tkMessageBox.showerror("Error", message)
-
-def info_message(message):
-    tkMessageBox.showinfo("Info", message)
-
-# show nickname screen
-def login_screen():
-    def on_select(event):
-    	widget = event.widget
-	global nickname    	
-	nickname = widget.get(widget.curselection())
-    	connect_server_screen()
-
-    def get_nickname(tb_nick):
-	global nickname
-    	nickname = tb_nick.get("1.0",'end-1c')
-    	if (nickname != '') and (' ' not in nickname) and len(nickname) <= 8:
-            write_names = open("nicknames", "a")
-            write_names.write("\n"+nickname)
-            connect_server_screen()
-        else:
-            error_message("your nickname is not valid")
-
-    read_names = open("nicknames", "r")
-    names = read_names.read().split()
-    global current_frame
-    current_frame = Tk()
-    current_frame.title("Enter Nickname")
-    lb_names = Listbox(current_frame, selectmode='single')
-    lb_names.bind('<<ListboxSelect>>', on_select)
-    for n in names:
-        lb_names.insert(END,n)
-    lb_names.pack()
-    btn_okay = Button(current_frame, text="ok", command=lambda : get_nickname(tb_nick), width=20)
-    btn_okay.pack({"side": "bottom"})
-    lbl_nickname = Label(current_frame, text="Your Nickname")
-    lbl_nickname.pack()
-    tb_nick = Text(current_frame, width=50, height=5)
-    tb_nick.pack()
-
-# connect to server screen
-def connect_server_screen():
-    def get_address_port(tb_addr, tb_port):
-    	addr = tb_addr.get("1.0", 'end-1c')
-    	port = tb_port.get("1.0", 'end-1c')
-	global nickname
-    	player = new_player(nickname, addr, port)
-    	if "error" in player:
-	    error_message(player["error"])
-    	else:
-	    global proxy
-	    proxy = player["proxy"]
-	    global client_id
-            client_id = player["client_id"]["client_id"]
-            session_list = get_session_list(proxy)
-            if "error" in session_list:
-                error_message(session_list["error"])
-            else:
-                multiplayer_game_screen(session_list) 
-    
-    global current_frame
-    current_frame.destroy()
-    current_frame = Tk()
-    current_frame.title("Enter Sudoku server address")
-    okay = Button(current_frame, text="ok", command=lambda:get_address_port(tb_addr, tb_port), width=20)
-    okay.pack({"side": "bottom"})
-    lbl_address = Label(current_frame, text="Server Address",font=("Arial", 10))
-    lbl_address.pack()
-    tb_addr = Text(current_frame, width=50, height=2, font=("Arial", 10))
-    tb_addr.pack()
-    lbl_port = Label(current_frame, text="Server Port",font=("Arial", 10))
-    lbl_port.pack()
-    tb_port = Text(current_frame, width=50, height=2,font=("Arial", 10))
-    tb_port.pack()
-
-# multiplayer game screen
-def multiplayer_game_screen(list_sessions):
-
-    def on_select(event):
-	widget = event.widget
-	global session_id
-	global proxy
-    	session_id = int(widget.get(widget.curselection()))
-        status = join_session(proxy, client_id, session_id)
-        if "error" in status:
-            error_message(status["error"])
-        else:
-	    global game
-	    if status["isAvailable"] and status["isGameStarted"]:
-	        game = status["game"]
-                start_game_screen(status["game"])
-	    elif status["isAvailable"] and not status["isGameStarted"]:
-		game = status["game"]
-		waiting_screen(status["game"])
-	    else:
-		info_message("Game is not available!")
-
-    global current_frame
-    current_frame.destroy()
-    current_frame = Tk()
-    current_frame.title("Multiplayer Game Dialog ")
-    lb_sessions = Listbox(current_frame,height = 5,font=("Arial", 10),selectmode='single')
-    lb_sessions.bind('<<ListboxSelect>>', on_select)
-    for session in list_sessions:
-        lb_sessions.insert(END, session["session_id"])
-    lb_sessions.pack()
-    btn_okay = Button(current_frame, text="New Session", command = new_session_screen, width=20)
-    btn_okay.pack({"side": "bottom"})
-
-# new session screen
-def new_session_screen():
-
-    def session(tb_player_number):
-    	player_number = tb_player_number.get("1.0", 'end-1c')
-    	if player_number is not None:
-	    global client_id
-	    global session_id
-	    global proxy
-            session = new_session(proxy, client_id, player_number)
-	    
-            if "error" in session:
-            	error_message(session["error"])
-            else:
-                status = join_session(proxy, client_id, session["session_id"])
-		global game
-		print status
-		game = status["game"]
-		if not status["isGameStarted"]:
-		    waiting_screen()
-		else:
-		    start_game_screen()
-    
-    global current_frame
-    current_frame.destroy()
-    current_frame = Tk()
-    current_frame.title("New Sudoku Solving Session")
-    lbl_player_number = Label(current_frame, text="Number of Players:")
-    lbl_player_number.pack()
-    tb_player_number = Text(current_frame, width=50, height=2)
-    tb_player_number.pack()
-    btn_okay = Button(current_frame, text="Ok", command = lambda: session(tb_player_number), width=20)
-    btn_okay.pack({"side": "bottom"})
-
-# wait screen
-def waiting_screen():
-    global current_frame
-    current_frame.destroy()
-    current_frame = Tk()
-    current_frame.title("New Sudoku Solving Session")
-    lbl_waiting = Label(current_frame, text="Waiting Players...")
-    lbl_waiting.pack()
-
-# start game screen
-def start_game_screen():
-    global sudoku_app
-    global current_frame
-    global game
-    current_frame.destroy()
-    current_frame = Tk()
-    sudoku_app = SudokuApp(current_frame, Sudoku())
-
+# Main method -----------------------------------------------------------------
 if __name__ == '__main__':
+    # Find the script absolute path, cut the working directory
     a_path = sep.join(abspath(argv[0]).split(sep)[:-1])
     # Append script working directory into PYTHONPATH
     path.append(a_path)
-    login_screen()
-    mainloop()
+    # Parsing arguments
+    parser = ArgumentParser(description="",
+                            version = "")
+    parser.add_argument('-l','--listenaddr', \
+                        help='Bind server socket to INET address, '\
+                        'defaults to %s' % DEFAULT_SERVER_INET_ADDR, \
+                        default=DEFAULT_SERVER_INET_ADDR)
+    parser.add_argument('-p','--listenport', \
+                        help='Bind server socket to UDP port, '\
+                        'defaults to %d' % DEFAULT_SERVER_PORT, \
+                        default=DEFAULT_SERVER_PORT)
+    args = parser.parse_args()
+
+    # TODO : Use default settings for server ip and port
+    try:
+        GUI().gui_start()
+    except KeyboardInterrupt:
+        LOG.info('Terminating client ...')
--- src/sudoku/game.py
+++ src/sudoku/game.py
@@ -1,5 +1,37 @@
-class Sudoku(object):
+def list_diff(lst1, lst2):
+    """Return lst1 - lst2.
+
+    list_diff(list<X>, list<X>) -> list<X>
+    The result is the list of entries in lst1 that are not in lst2.
+
+    """
+    diff = []
+    for e in lst1:
+        if not e in lst2:
+            diff.append(e)
+    return diff
+
+def list_intersection(lst1, lst2):
+    """Return the intersection.
+
+    list_intersection(list<X>, list<X>) -> list<X>
+    The result is the list of all entries in both lst1 and lst2.
+
+    """
+    inter = []
+    for e in lst1:
+        if e in lst2:
+            inter.append(e)
+    return inter
+
+class Sudoku:
+    """A Sudoku game"""
+
+    # all the valid choices
+    all_choices = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
     def __init__(self):
+     
         self.sudoku_board = [["4","3","5","2","6","9","7","8","1"],
                ["6","8","2","5","7","1","4","9","3"],
                ["1","9","7","8","3","4","5","6","2"],
@@ -9,7 +41,7 @@ class Sudoku(object):
                ["5","1","9","3","2","6","8","7","4"],
                ["2","4","8","9","5","7","1","3","6"],
                ["7","6","3","4","1","8","2","5","9"]]
-        self.sudoku_puzzle = [[" "," "," ","2","6"," ","7"," ","1"],
+        self._game = [[" "," "," ","2","6"," ","7"," ","1"],
                ["6","8"," "," ","7"," "," ","9"," "],
                ["1","9"," "," "," ","4","5"," "," "],
                ["8","2"," ","1"," "," "," ","4"," "],
@@ -19,15 +51,61 @@ class Sudoku(object):
                [" ","4"," "," ","5"," "," ","3","6"],
                ["7"," ","3"," ","1","8"," "," "," "]]
 
+    def get_row(self, r):
+      
+        return self._game[r]
+
+    def get_column(self, c):
+       
+        column = []
+        for r in self._game:
+            column.append(r[c])   
+        return column
+
+    def get_block(self, r, c):
+       
+        block = []
+        for row in range(3*int(r), 3*int(r)+3):
+            block.extend(self._game[row][3*int(c):3*int(c)+3])
+        return block
+
+    def get_entry(self, r, c):
+        return self._game[r][c]
+
+    def set_entry(self, r, c, v):
+        self._game[r][c] = v
+
+    def __getitem__(self, key):
+        r,c = key
+        return self._game[r][c]
+
+    def __setitem__(self, key, val):
+        r,c = key
+        self.set_entry(r,c,val)
+   
+    def choices(self, r, c):
+        block_row = r / 3
+        block_col = c / 3
+        row_choices = list_diff(Sudoku.all_choices, self.get_row(r))
+        col_choices = list_diff(Sudoku.all_choices, self.get_column(c))
+        block_choices = list_diff(Sudoku.all_choices, 
+                                  self.get_block(block_row, block_col))
+        choices = list_intersection(row_choices, col_choices)
+        choices = list_intersection(choices, block_choices)
+        return choices
+
     def check (self, i, j, value):
         if self.sudoku_board[i][j] == value:
-            self.sudoku_puzzle[i][j] = value
-            if self.sudoku_board == self.sudoku_puzzle:
+            self._game[i][j] = value
+            if self.sudoku_board == self._game:
                 return 1, True
             return 1, False
         else:
             return -1, False
+
     def get_puzzle (self):
-        return self.sudoku_puzzle
+        return self._game
+
     def get_board (self):
         return self.sudoku_board
+
